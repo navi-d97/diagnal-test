fd8efdbe84ed5f2f89479b4d4c705d20a649a533
LAPTOP-67AOG24S\dnavi <dnavin497@gmail.com>
2021-10-19 21:06:26 +0530

    added nav bar

---
+++ src/components/TopBar/index.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import './topBar.css';
+
+import navBarGradiant from '../../assets/nav_bar.png';
+import searchIcon from '../../assets/search.png';
+
+type TopBarProps = {
+    onSearch: (searchString: string) => void;
+    searchValue?: string;
+}
+
+function TopBar(props:TopBarProps) {
+    const {searchValue,onSearch} = props;
+    const [searchString, setSearchString] = useState(searchValue || "");
+    return (
+        <div className="sticky z-10 nav-bar flex flex-row bg-transparent">
+            <img src={navBarGradiant} alt="" className="absolute z-10 bg-transparent"/>
+            <input
+                value={searchString}
+                className="z-10 h-full w-11/12 ml-2 bg-transparent text-white text-xl"
+                onChange={(e)=>setSearchString(e.target.value)}
+            />
+            <button
+                className="z-10 w-1/12 h-full mr-2"
+                onClick={()=>onSearch(searchString)}
+            >
+                <img src={searchIcon} alt="search" className="object-contain"/>
+            </button>
+        </div>
+    )
+}
+
+export default React.memo(TopBar);
--- src/pages/contentListing.tsx
+++ src/pages/contentListing.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import List from '../components/list'
+import TopBar from '../components/TopBar'
 
 export default function ContentListing() {
     return (
-        <List />
+        <div className="flex-1 h-screen">
+            <TopBar onSearch={(value)=>{console.log(value)}} />
+            <List />
+        </div>
     )
 }
